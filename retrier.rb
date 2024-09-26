3e9ca52d4584b0d1f2082fc32980d7673cb2e1a0
Daniel Ransom <dantheman0207@users.noreply.github.com>
2024-09-26 22:30:34 +0200

    set hash correctly

--- retrier.rb
+++ retrier.rb
@@ -2,7 +2,7 @@ class Retrier < Formula
     desc "A tool for retrying commands until they succeed"
     homepage "https://github.com/dantheman0207/retrier"
     url "https://github.com/dantheman0207/retrier/archive/v0.0.1.tar.gz"
-    sha256 "ec0d8ee98838d18df0c5aa7f0bf8d9589b3778540fb1f427a95605904450dce5"
+    sha256 "d8d6c132eab0323aaf4ffd33f58e8fa3b065d33fa793e83cb0304a02a8283902"
     license "MIT"
   
     depends_on "go" => :build
