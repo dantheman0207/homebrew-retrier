3419b09d362c9d9a4826f2fe621dcd5c027bf06e
Daniel Ransom <dantheman0207@users.noreply.github.com>
2025-03-10 14:28:37 +0100

    Update Retrier formula to version 0.1.0    Changes URL to latest release tarball

--- retrier.rb
+++ retrier.rb
@@ -1,7 +1,7 @@
 class Retrier < Formula
     desc "A tool for retrying commands until they succeed"
     homepage "https://github.com/dantheman0207/retrier"
-    url "https://github.com/dantheman0207/retrier/archive/v0.0.1.tar.gz"
+    url "https://github.com/dantheman0207/retrier/archive/v0.1.0.tar.gz"
     sha256 "d8d6c132eab0323aaf4ffd33f58e8fa3b065d33fa793e83cb0304a02a8283902"
     license "MIT"
   
