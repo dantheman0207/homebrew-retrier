5a179d10275fe82032b59ebd1593f8e70eba788b
Daniel Ransom <dantheman0207@users.noreply.github.com>
2025-04-16 17:21:47 +0200

    fix sha256 for real this time

--- retrier.rb
+++ retrier.rb
@@ -2,7 +2,7 @@ class Retrier < Formula
     desc "A tool for retrying commands until they succeed"
     homepage "https://github.com/dantheman0207/retrier"
     url "https://github.com/dantheman0207/retrier/archive/v0.1.4.tar.gz"
-    sha256 "e6f4cad1355d269d634caf24bdb080b424c2a048cb453f4a07891f8f98b81bac"
+    sha256 "ebd097c6edd093777c2e0b3269eaaa6f4378eeeb5fe38e8ffea515205a528fc2"
     license "MIT"
   
     depends_on "go" => :build
