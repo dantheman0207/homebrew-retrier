16fc2f997be1689890404bdebf59116d2efab9b0
Daniel Ransom <dantheman0207@users.noreply.github.com>
2025-04-29 00:02:19 +0200

    Release 0.1.11

--- retrier.rb
+++ retrier.rb
@@ -2,7 +2,7 @@ class Retrier < Formula
     desc "A tool for retrying commands until they succeed"
     homepage "https://github.com/dantheman0207/retrier"
     url "https://github.com/dantheman0207/homebrew-retrier/releases/download/v0.1.13/retrier-v0.1.13-darwin-arm.tar.gz"
-    sha256 "522dc1f861652b0080b2add4df08d4feb3ba6297728f24a597f0bc289a1cae2e"
+    sha256 "728a000c403ba3762218e0c9342884f6b7daffaa8c9866f2befb9bed57c5192c"
     license "MIT"
   
     depends_on "go" => :build
